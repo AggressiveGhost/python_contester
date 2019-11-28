061e8916e5d0873fed16df9e25b417963d9f1125
AggressiveGhost <42280068+AggressiveGhost@users.noreply.github.com>
2019-11-28 13:32:43 +0600

    glu

--- myFirstApp/models.py
+++ myFirstApp/models.py
@@ -1,12 +1,21 @@
-from django.db import models
+from django.db import *
 from django.urls import *
 from django.contrib.auth.models import *
 from django.conf import settings
-from django.contrib.auth.models import User
+from datetime import datetime    
 
-# Create your models here.
+# ---> User Model
+class UserProfileInfo(models.Model):
+    user        = models.OneToOneField(User,on_delete=models.CASCADE)
+    link        = models.URLField(blank=True)
+    avatar      = models.ImageField(upload_to='profile_pics',blank=True,default='profile_pics/user.png')
+
+    def __str__(self):
+        return self.user.username
 
 
+
+# ---> Task model
 class Task(models.Model):
     task_name   = models.CharField(max_length = 1000)
     task_img    = models.ImageField(upload_to='task_img/', default='NULL')
@@ -16,49 +25,43 @@ class Task(models.Model):
     def __str__(self):
         return str("{0} {1}".format(self.id ,self.task_name))
 
-    def getCurrentTask(self):
-        return reverse_lazy('currentTask', kwargs={'currentId':self.id})
 
 
-
-
+# ---> Code model
 class Code(models.Model):
     task        = models.ForeignKey(Task, on_delete = models.CASCADE)
     user        = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     task_code   = models.TextField(default="")
+    date        = models.DateTimeField(default=datetime.now, blank=True)
+    score       = models.IntegerField(default=0)
     isSolved    = models.BooleanField(default=False)
 
     def __str__(self):
-        return str(self.user.username)
-    
-    
- 
+        return self.user.username + "___" + self.date.strftime("%H:%M:%S")
 
 
-class UserProfileInfo(models.Model):
-    user        = models.OneToOneField(User,on_delete=models.CASCADE)
-    link        = models.URLField(blank=True)
-    avatar      = models.ImageField(upload_to='profile_pics',blank=True,default='NULL')
 
-def __str__(self):
-  return self.user.username
-
-
+# ---> Question model
 class Question(models.Model):
     user        = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title       = models.TextField(default="")
     text        = models.TextField(default="")
     img         = models.ImageField(upload_to='Q_img/', default='NULL')
+    date        = models.DateTimeField(default=datetime.now, blank=True)
     isSolved    = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.title)
 
-class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    text        = models.TextField(default="")
-    author      = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+
+# ---> Answer model
+class Answer(models.Model):
+    question    = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text        = models.TextField(default="")
+    date        = models.DateTimeField(default=datetime.now, blank=True)
+    author      = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
     def __str__(self):
         return str(self.author.username)
     
--- myFirstApp/views.py
+++ myFirstApp/views.py
@@ -6,17 +6,28 @@ from django import *
 from .models import *
 from .forms import *
 from django.contrib.auth.views import *
+from random import *
+from django.db.models import Count
 
 # Create your views here.
 
-def index(request): # shows 4 last popular tasks by number of clics
-    taskTitle = 'Polular Tasks'
-    allTasks = Task.objects.all()
-    count = int(0)
-    for i in allTasks:
-        count += 1
+def index(request): 
+    # ---> Quantity of Tasks
+    tasks = Task.objects.all().count() 
+    # ---> Quantity of Users
+    users = UserProfileInfo.objects.all().count() 
+    # ---> Quantity of Solved Tasks
+    solved = Code.objects.filter(isSolved = False).count() 
+     # ---> Last 3 popular Tasks
     popular = Task.objects.filter(clicks__gt = 3).order_by('-clicks')[:3]
-    return render(request, 'myFirstApp/popTask.html', context={'title':taskTitle,'task':popular, 'countTask':count})
+
+    dic = {
+        'countTask':tasks,
+        'countUser':users,
+        'countSolved':solved
+    }
+
+    return render(request, 'myFirstApp/popTask.html', context={'title':'Polular Tasks','task':popular, 'dic':dic})
 
 
 def forum(requrest):
@@ -24,9 +35,18 @@ def forum(requrest):
 
 
 def currentTask(request, task_id):
-    taskEvery = Task.objects.get(id = task_id)
-    taskEvery.clicks += 1
-    return render(request, 'myFirstApp/everyTask.html', {'everyTask':taskEvery})#expection change key
+    currentTask = Task.objects.get(id = task_id)
+    # ---> Increase number of clics
+    currentTask.clicks += 1
+    # ---> Codes of current User
+    code = Code.objects.filter(user = request.user, task = currentTask).order_by('-date')
+    
+    dic = {
+        'currentTask' : currentTask,
+        'code' : code
+    }
+    
+    return render(request, 'myFirstApp/everyTask.html', {'dic':dic}) 
 
 
 
@@ -37,6 +57,20 @@ def moreProblems(request):
 
 
 def rating(request):
+
+    def getScore(c:Code):
+        n = Count(c.score)
+        return n
+
+    users = UserProfileInfo.objects.all()
+    code = Code.objects.filter(score__gt = 1)
+    # print(code, "------")    
+
+    for user in users:
+        code = Code.objects.filter(score__gt = 1, user = user)
+        
+        print(code)
+
     return render(request, 'myFirstApp/rayting.html')
 
 def signIn(request):
@@ -47,7 +81,9 @@ def signUp(request):
     return render(request, 'myFirstApp/signUp.html')
 
 def userpage(request):
-    return render(request, 'myFirstApp/userPage.html')
+    print(request.user)
+    user = request.user
+    return render(request, 'myFirstApp/userPage.html', {'user':user})
 
 
 
@@ -72,6 +108,7 @@ def register(request):
             user = user_form.save()
             user.set_password(user.password)
             user.save()
+            login(request,user)
             profile = profile_form.save(commit=False)
             profile.user = user
             if 'avatar' in request.FILES:
@@ -79,6 +116,8 @@ def register(request):
                 profile.profile_pic = request.FILES['avatar']
             profile.save()
             registered = True
+            # ---> reverces to the main page
+            return HttpResponseRedirect(reverse_lazy('index'))
         else:
             print(user_form.errors,profile_form.errors)
     else:
@@ -118,14 +157,19 @@ def addCode(request,task_id):
         code = request.POST.get("code_text")
         print(code,request.POST.get('username'))
 
-        co = Code(user = User.objects.get(username= request.POST.get('username')), task_code = code, task = Task.objects.get(pk=task_id))
+        score = int(0)
+        score = randint(60, 100)
+        print(score)
+
+        co = Code(user = User.objects.get(username= request.POST.get('username')), task_code = code, task = Task.objects.get(pk=task_id), score = score)
         print(co)
         co.save()
         #everyTask
         taskEvery = Task.objects.get(pk=task_id)
         # return render(request, 'myFirstApp/popTask.html', {'everyTask':taskEvery})#expection change key
         # return HttpResponseRedirect(reverse_lazy('currentTask/{}/'.format(taskEvery.id)))
-        return currentTask(request, task_id)
+        return currentTask(request, task_id) #баг
+        # return HttpResponseRedirect(reverse_lazy('currentTask', args=(task_id)))
     except:
         return HttpResponse('unSuccess')
 
